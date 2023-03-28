138e34e5a030e5fd9bfd8944f0b35340886448a7
vikmaamaa <maamaavictor@yahoo.com>
2023-03-28 03:05:12 +0100

    feat: authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contac
 Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 
 Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
